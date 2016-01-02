0d41413f6d4ecf6fa82c7eb22e6cd957ad64099f
Chris Dellinger <cdellinger@gmail.com>
2016-01-02 14:11:03 -0500

    test: fixing test label

--- test/index_test.js
+++ test/index_test.js
@@ -1,4 +1,4 @@
-describe("Standard Entryway DocumentDB Provider Tests", function () {
+describe("Standard Entryway DynamoDB Provider Tests", function () {
 	// Execute the standard provider test that is contained within Entryway, all providers should be able to support these tests
     require('entryway').ProviderTest(require('../lib/index'));
 });
